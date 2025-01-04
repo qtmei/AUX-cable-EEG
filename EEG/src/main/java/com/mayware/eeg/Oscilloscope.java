87852c2d0b85582a129d617fd88233c1b41f8c17
May <92554133+qtmei@users.noreply.github.com>
2025-01-03 18:33:37 -0600

    Update Oscilloscope.java

--- EEG/src/main/java/com/mayware/eeg/Oscilloscope.java
+++ EEG/src/main/java/com/mayware/eeg/Oscilloscope.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mayware.eeg;
-
-/*
- * @author May Fontenot
- */
 
 public class Oscilloscope extends Thread
 {   
